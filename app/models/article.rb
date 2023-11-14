1aedbd9bc92a29b9aed4eda90bba950343b88b4d
Mithilesh Saddul <saddulmithilesh@gmail.com>
2023-11-14 05:27:05 -0500

    first commit

---
+++ app/models/article.rb
@@ -0,0 +1,12 @@
+class Article < ApplicationRecord
+    has_many :comments
+  
+    class Article < ApplicationRecord
+        include Visible
+      
+        has_many :comments
+      
+        validates :title, presence: true
+        validates :body, presence: true, length: { minimum: 10 }
+    end
+end
